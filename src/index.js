1d9f1983a0893bc21334ce715b660348e7351042
DenisGerasimenko <gerasimenkodenis7@gmail.com>
2021-03-15 23:49:24 +0300

    feat: add functions for calculate max, min and avg means of array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
+exports.min = function min(array) {
+    if (array === undefined || array == false) {
+        return 0;
+    } else {
+        let arr = Math.min(...array);
+        return arr;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array == false) {
+        return 0;
+    } else {
+        let arr = Math.max(...array);
+        return arr;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array == false) {
+        return 0;
+    } else {
+        let sum = 0;
+        let average = 0;
+        for (i = 0; i < array.length; i++) {
+            sum += array[i];
+            average = sum / array.length;
+        }
+        return average;
+    }
+};
